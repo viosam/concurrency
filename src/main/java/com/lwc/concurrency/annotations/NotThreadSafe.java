a966cd3291ae3fbb45d90d76412099a6676943de
viosam <123@163.com>
2019-05-28 10:44:56 +0800

    add annotations

---
+++ src/main/java/com/lwc/concurrency/annotations/NotThreadSafe.java
@@ -0,0 +1,17 @@
+package com.lwc.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标识线程不安全的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+
+}
